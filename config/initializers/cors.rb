03a8f9fde5b31dcaae8b008cbc76dbb614bc9822
NickSealer <nickfontechav@gmail.com>
2024-04-09 14:42:47 -0500

    config cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*' # 'http://localhost:3000'
-    resource '/api/v1/*', headers: :any, methods: %i[get post patch put delete]
+    resource '/api/v1/*', headers: 'ACCESS_KEY', methods: %i[get post patch put delete]
+    resource '/graphql', headers: 'ACCESS_KEY', methods: :post
   end
 end
